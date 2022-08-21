7e057a210daffa69c93dcd217a06780ecb84b7f9
CarlosCubNed <97036823+carloscfgos1980@users.noreply.github.com>
2022-08-21 09:18:24 +0200

    Add files via upload

---
+++ State Practice II/App.js
@@ -0,0 +1,35 @@
+import React from "react"
+
+// Given a stateless functional component, add state to it
+// state should have a property called `isLoggedIn` which is a boolean
+// (true if logged in, false if not)
+// Then, give your best shot at rendering the word "in" if the user is logged in
+// or "out" if the user is logged out.
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isLoggedIn: false
+        }
+    }
+
+    // isLoggedIn is a boolean function so we dont need to === true because the is for default. It will work anyway. Ex:
+    //if (this.state.isLoggedIn === true)
+
+    render() {
+        let wordDisplay
+        if (this.state.isLoggedIn) {
+            wordDisplay = "in"
+        } else {
+            wordDisplay = "out"
+        }
+        return (
+            <div>
+                <h1>You are currently logged {wordDisplay}</h1>
+            </div>
+        )
+    }
+}
+
+export default App
